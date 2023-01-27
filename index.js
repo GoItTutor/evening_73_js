097a0dbafacdc762d6b84f9b93cf46e478fbf082
GoItTutor <sheva09a@gmail.com>
2023-01-27 13:23:03 +0200

    module-03-lesson-02

--- index.js
+++ index.js
@@ -1,138 +1,129 @@
 /*
-  1. Деструктуризація
-
-  Перепиши функцію так, щоб вона приймала один об'єкт параметрів
-  замість набору незалежних аргументів
+  1. Операція spread як заміна concat і slice
 */
 
-// function calcBMI(weight, height) {
-//   const numericWeight = Number(weight.replace(',', '.'));
-//   const numericHeight = Number(height.replace(',', '.'));
+//*  Копія масиву
 
-//   return Number((numericWeight / numericHeight ** 2).toFixed(1));
-// }
+// const array = [1, 2, 3, 4, 5];
 
-// console.log(calcBMI('68,3', '1.65'));
-// console.log(calcBMI('118,3', '1.95'));
+//* Об'єднання масивів через spread замість concat
+
+// const array1 = [1, 2, 3, 4, 5];
+// const array2 = [6, 7, 8, 9];
 
 /*
-  2. Деструктуризація
+  2. Копія об'єкту
 
-  Перепиши функцію так, щоб вона приймала один об'єкт параметрів
-  замість набору незалежних аргументів
+  - Перезапис однакових ключів при копіюванні
 */
 
-// function printContactsInfo(names, phones) {
-//   const nameList = names.split(',');
-//   const phoneList = phones.split(',');
-
-//   for (let i = 0; i < nameList.length, i < phoneList.length; i += 1) {
-//     console.log(`${nameList[i]}: ${phoneList[i]}`);
-//   }
-// }
+// const user = {
+//   firstName: 'Andrii',
+//   lastName: 'Shevchuk',
+//   age: 30,
+// };
 
 /*
-  3. Глибока деструктуризація
+  3. Spread + Rest
 
-  Перепиши функцію так, щоб вона приймала один об'єкт параметрів
-  замість набору незалежних аргументів
+  Напишіть функцію getMinNumber яка приймає довільну кількість чисел
+  та повертає найменше із них
 */
-
-// function getBotReport(companyName, repairBots, defenceBots) {
-//   return `${companyName} has ${repairBots + defenceBots} bots in stock`;
-// }
-
-// Було так:
-// console.log(getBotReport('Cyberdyne Systems', 150, 50))
-
-// Має стати так:
-// console.log(getBotReport({
-//     companyName: 'Cyberdyne Systems',
-//     bots: {
-//         repairBots: 150,
-//         defenceBots: 50,
-//     }
 
 /*
-  4. Деструктуризація
-
-  Напиши функцію щоб вона приймала об'єкт параметрів із властивостями 
-  companyName і stock та виводила репорт про кількість товарів на 
-  складі будь-якої компанії.
-*/
-
-// console.log(
-//     getStockReport({
-//         companyName: 'Cyberdyne Systems',
-//         stock: {
-//             repairBots: 150,
-//             defenceBots: 50,
-//         },
-//     }),
-// ); // "Cyberdyne Systems has 200 items in stock"
-
-// console.log(
-//     getStockReport({
-//         companyName: 'Belacci',
-//         stock: {
-//             shoes: 20,
-//             skirts: 10,
-//             hats: 5,
-//         },
-//     }),
-// ); // "Belacci has 35 item in stock"
-
-/* 
-  5. Spread
-  Доповни функцію createContact(partialContact) так, щоб вона повертала 
-  новий об'єкт контакту з доданою властивістю id, а також list зі 
-  значенням "default" якщо у partialContact немає такої властивості.
-*/
-
-// function createContact(partialContact) {
-// }
-
-// function generateId() {
-//     return '_' + Math.random().toString(36).substr(2, 9);
-// }
-
-// console.log(
-//     createContact({
-//         name: 'Mango',
-//         email: 'mango@mail.com',
-//         list: 'friends',
-//     }),
-// );
-// console.log(
-//     createContact({
-//         name: 'Poly',
-//         email: 'poly@hotmail.com',
-//     }),
-// );
 
 /*
-  6. Rest
-  Напиши функцію transformUsername(user) так, щоб вона повертала новий 
-  об'єкт із властивістю fullName, замість firstName та lastName зі збережнням
-  усіх інших полів
+  4. Деструктуризація об'єктів, які проблеми вирішує
+
+  Напишіть деструктуризуюче присвоювання, яке:
+
+  властивість firstName присвоїть у змінну firstName
+
+  властивість age присвоїть у змінну userAge
+
+  властивість isAdmin присвоїть у змінну isAdmin (false, по дефолту
+  якщо, у об'єкта данної властивості не буде)
+
+  всі інші властивості зібрати у об'єкт features
 */
 
-// console.log(
-//     transformUsername({
-//         id: 1,
-//         firstName: 'Jacob',
-//         lastName: 'Mercer',
-//         email: 'j.mercer@mail.com',
-//         friendCount: 40,
-//     }),
-// );
+// const user = {
+//   firstName: 'Andrii',
+//   lastName: 'Shevchuk',
+//   age: 30,
+//   eyeColor: 'green',
+//   hobbies: [],
+// };
 
-// console.log(
-//     transformUsername({
-//         id: 2,
-//         firstName: 'Adrian',
-//         lastName: 'Cross',
-//         email: 'a.cross@hotmail.com',
-//         friendCount: 20,
-//     }),
-// );
+/*
+  5. Глубока деструктуризація об'єктів
+
+  Потрібно дістати значення усіх властивостей, включно
+  з вкладеними у об'єкт langs
+*/
+
+// const team = {
+//   number: 4,
+//   flag: './images/flag.jpg',
+//   employees: ['Anton', 'Oleg', 'Ronnie', 'Carr'],
+//   langs: {
+//     original: 'uk',
+//     secondary: 'en',
+//   },
+// };
+
+/*
+  6. Деструктуризація масивів
+*/
+
+/*
+  Дістати перший та третій елементи масиву, всі інші після 
+  третього занести в окремий масив
+*/
+
+// const names = [
+//   'Herbert Todd',
+//   'Belle Soto',
+//   'Roger Marsh',
+//   'Ethan Lindsey',
+//   'Leo Dicaprio',
+//   'Arnold Polo',
+// ];
+
+/*
+  Дістати значення кольорів та властивість alpha
+*/
+
+// const rgb = [0, 255, 34];
+
+/*
+  У нас є об'єкт salaries з зарплатами
+
+  Створіть функцію topSalary(salaries), яка повертає ім'я найбільш
+  високооплачуваного працівника
+
+  Якщо об'єкт salaries пустий то повернути null
+
+  Якщо декілька людей з одною тою самою зарплатою є найбільш високооплачуваними, 
+  можна повернути будь-якого з них
+ 
+  Використовуйте Object.entries та деструктуризацію, щоб перебрати пари ключ/значение.
+*/
+
+// const salaries = {
+//   John: 100,
+//   Pete: 300,
+//   Mary: 250,
+// };
+
+/*
+  7. Паттерн 'об'єкт параметрів', яку проблему вирішує
+
+  Перепишіть функцію sayHi, щоб вона відповідала заданому паттерну
+*/
+
+// function sayHi(name, age, height, weight) {
+//   console.log(
+//     `Hi, ${name}! Your age is ${age}, your heigth is ${height}, your weight is ${weight}`
+//   );
+// }
