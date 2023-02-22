6f671631212f85053939c787285ef0d26d8404d0
GoItTutor <sheva09a@gmail.com>
2023-02-22 20:38:20 +0100

    module-06-lesson-01

--- gallery.js
+++ gallery.js
@@ -10,6 +10,35 @@
   </li>
 */
 
+// const listEl = document.querySelector('.gallery');
+
+// // console.log(listEl);
+
+// const listItemEl = document.createElement('li');
+
+// listItemEl.classList.add('gallery-item');
+
+// // console.log(listItemEl);
+
+// const listAnchorEl = document.createElement('a');
+
+// listAnchorEl.href = '#';
+
+// // console.log(listAnchorEl);
+
+// listItemEl.append(listAnchorEl);
+
+// const listImgEl = document.createElement('img');
+
+// listImgEl.src = 'https://picsum.photos/id/237/200/300';
+// listImgEl.alt = 'Labrador';
+
+// listAnchorEl.append(listImgEl);
+
+// console.log(listItemEl);
+
+// listEl.append(listItemEl);
+
 /* 
   6. Властивості innerHtml і textContent
 
@@ -22,6 +51,18 @@
     </a>
   </li>
 */
+
+// const listEl = document.querySelector('.gallery');
+
+// listEl.innerHTML = `
+//   <li class="gallery-item">
+//     <h1>Some text</h1>
+//       <a href="#">
+//         <img src="https://picsum.photos/id/237/200/300" alt="Labrador">
+//       </a>
+//   </li>`;
+
+// listEl.textContent = `<h1>3433</h1>`;
 
 /*
   7. Створити галерею і вставиити у елемент з класом gallery через
@@ -70,6 +111,23 @@ const pictures = [
 ];
 
 // Написати функцію яка створить розмітку картки
-const makeGalleryCard = ({ width, height, url, alt }) => {};
+const makeGalleryCard = ({
+  width,
+  height,
+  url,
+  alt,
+}) => `<li class="gallery-item">
+    <a href="#">
+      <img src="${url}" alt="${alt}" height="${height}" width="${width}">
+    </a>
+  </li>`;
 
 // Cтворити масив із розмітки і вставити у список .gallery
+const listEl = document.querySelector('.gallery');
+
+const markup = pictures.map((data) => makeGalleryCard(data)).join('');
+
+// listEl.innerHTML = markup;
+
+listEl.insertAdjacentHTML('afterbegin', markup);
+console.log(markup);
--- index.js
+++ index.js
@@ -10,6 +10,14 @@
   Навігація по DOM дереву (children, etc.)
 */
 
+// const element = document.querySelectorAll('*');
+
+// console.log(element);
+
+// const paragraphEl = document.querySelector('.description');
+
+// console.log(paragraphEl.nextElementSibling.children);
+
 /* 
   2. Доступ до атрибутів у елемента
 
@@ -26,14 +34,44 @@
 
 // const imgEl = document.querySelector('.some-img');
 
+// imgEl.setAttribute('src', 'https://picsum.photos/id/239/200/300');
+
+// console.log(imgEl.getAttribute('alfsdfdst'));
+
+// imgEl.removeAttribute('src');
+
+// imgEl.src = 'https://picsum.photos/id/239/200/300';
+
 /*
   3. Об'єкт style, властивість cssText
 
   show on heading with class main-title
 */
 
+// const titleEl = document.querySelector('#title');
+
+// console.log(titleEl);
+
+// titleEl.style.color = 'green';
+// titleEl.style.backgroundColor = 'tomato';
+// titleEl.style.fontSize = '60px';
+
+// titleEl.style.cssText =
+//   'color: green; background-color: tomato; font-size: 60px';
+
 /*
   4. Об'єкт classList та його методи (add, remove, toggle, contains)
 
   take dissapear and red classes from main.css
 */
+
+// const imgEl = document.querySelector('.some-img');
+
+// imgEl.classList.add('dissapear');
+// imgEl.classList.remove('dissapear');
+
+// imgEl.classList.toggle('dissapear');
+
+// console.log(imgEl.classList.contains('some-img'));
+
+// imgEl.classList.replace('some-img', 'red');
