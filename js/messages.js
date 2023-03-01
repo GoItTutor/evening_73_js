a555c6ddd752bc70b0e8484ed5484bc3a703c3dd
GoItTutor <sheva09a@gmail.com>
2023-03-01 20:37:26 +0100

    module-07-lesson-01

--- js/messages.js
+++ js/messages.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const messagesWrapperEl = document.querySelector('.js-messages-wrapper');
+
+const handleMessagesClick = ({ target }) => {
+  if (target.nodeName !== 'BUTTON') {
+    return;
+  }
+
+  const messageEl = target.closest('.message');
+
+  messageEl.remove();
+};
+
+messagesWrapperEl.addEventListener('click', handleMessagesClick);
--- js/pallet.js
+++ js/pallet.js
@@ -14,8 +14,23 @@ const paintElements = () => {
     const color = randomColor();
 
     element.style.backgroundColor = color;
-    // element.dataset.color = color;
+
+    element.dataset.color = color;
   });
 };
 
 paintElements();
+
+const handlePalettleClick = ({ target }) => {
+  if (target.nodeName !== 'LI') {
+    return;
+  }
+
+  const currentColor = target.dataset.color;
+
+  const outputEl = target.firstElementChild;
+
+  outputEl.textContent = currentColor;
+};
+
+palletEl.addEventListener('click', handlePalettleClick);
