ca6082e1718abcbdbf552f7ab1ced1116265e078
GoItTutor <sheva09a@gmail.com>
2023-03-02 20:40:22 +0100

    module-07-lesson-02

--- js/lazy-gallery.js
+++ js/lazy-gallery.js
@@ -4,36 +4,58 @@ const galleryEl = document.querySelector('.gallery');
 const bannerImgEl = document.querySelector('.banner__img');
 const galleryImgEls = document.querySelectorAll('.gallery__img');
 
-// const addSrcAttrToImg = () => {
-//   const imageEls = document.querySelectorAll('img');
+galleryEl.addEventListener('click', event => {
+  event.preventDefault();
 
-//   imageEls.forEach(el => {
-//     el.src = el.dataset.src;
-//   });
-// };
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
 
-// const createLazySizesScript = () => {
-//   const script = document.createElement('script');
+  const bannerUrl = event.target.dataset.bannerUrl;
 
-//   script.src =
-//     'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js';
+  bannerImgEl.src = bannerUrl;
+});
 
-//   script.setAttribute(
-//     'integrity',
-//     'sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ=='
-//   );
+galleryImgEls.forEach(picture => {
+  picture.addEventListener(
+    'load',
+    ({ target }) => {
+      target.classList.add('appear');
+    },
+    { once: true }
+  );
+});
 
-//   script.setAttribute('crossorigin', 'anonymous');
+const addSrcAttrToImg = () => {
+  const imageEls = document.querySelectorAll('img');
 
-//   script.setAttribute('referrerpolicy', 'no-referrer');
+  imageEls.forEach(el => {
+    el.src = el.dataset.src;
+  });
+};
 
-//   return script;
-// };
+const createLazySizesScript = () => {
+  const script = document.createElement('script');
 
-// if ('loading' in HTMLImageElement.prototype) {
-//   console.log('Аттрибут loading поддерживается');
-//   addSrcAttrToImg();
-// } else {
-//   console.log('Аттрибут loading не поддерживается');
-//   document.body.append(createLazySizesScript());
-// }
+  script.src =
+    'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js';
+
+  script.setAttribute(
+    'integrity',
+    'sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ=='
+  );
+
+  script.setAttribute('crossorigin', 'anonymous');
+
+  script.setAttribute('referrerpolicy', 'no-referrer');
+
+  return script;
+};
+
+if ('loading' in HTMLImageElement.prototype) {
+  console.log('Аттрибут loading поддерживается');
+  addSrcAttrToImg();
+} else {
+  console.log('Аттрибут loading не поддерживается');
+  document.body.append(createLazySizesScript());
+}
